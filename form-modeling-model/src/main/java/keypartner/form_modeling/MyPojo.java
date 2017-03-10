ad488f419c5df87853138f1aa7b445d8e45ecdd0
claudio.campisi <claudio.campisi@keypartner.it>
2017-03-10 16:40:24 +0100

    addef mdoel

---
+++ form-modeling-model/src/main/java/keypartner/form_modeling/MyPojo.java
@@ -0,0 +1,40 @@
+package keypartner.form_modeling;
+
+import java.io.Serializable;
+
+public class MyPojo implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6040069096536772037L;
+
+	private String field1;
+	private String field2;
+	private int field3;
+	public String getField1() {
+		return field1;
+	}
+	public void setField1(String field1) {
+		this.field1 = field1;
+	}
+	public String getField2() {
+		return field2;
+	}
+	public void setField2(String field2) {
+		this.field2 = field2;
+	}
+	public int getField3() {
+		return field3;
+	}
+	public void setField3(int field3) {
+		this.field3 = field3;
+	}
+	
+	
+	
+
+	
+	
+	
+
+}
